2a35b0de5790325ebc3498c1413c4bd4d2dbde55
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-08-24 17:26:52 +0200

    Ace-ify rust code blocks

--- resources/js/application.js
+++ resources/js/application.js
@@ -1,6 +1,12 @@
-(function($, Elm){
+(function($, Elm, ace){
     $.deck('.slide');
-    $(".rust").load_snippets();
+    $(".rust")
+        .load_snippets()
+        .each(function(index, element){
+            var editor = ace.edit(element);
+            editor.setTheme("ace/theme/chrome");
+            editor.getSession().setMode('ace/mode/rust');
+        });
 
     var plain = $('#plain-brainbow')[0];
     Elm.Brainbow.embed(plain);
@@ -8,4 +14,4 @@
     Elm.Brainbow.embed(word);
     var solve = $('#solve-brainbow')[0];
     Elm.Brainbow.embed(solve);
-})(jQuery, Elm);
+})(jQuery, Elm, ace);
