741aefbf60058b56ae80cd14218a9d054c81645a
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-09-03 09:56:54 +0200

    Create elements to execute code

--- resources/js/load-snippets.js
+++ resources/js/load-snippets.js
@@ -6,9 +6,13 @@
             data: 'snippet'
         }, options);
         this.each(function(index, element){
+            var t = $("<textarea>");
+            var b = $("<button>"); b.text("Run");
+            var r = $("<div>");
+            [t, b, r].forEach(function(child){ $(element).append(child); });
             var url = settings.location + $(element).data(settings.data);
             $.ajax(url).done(function(data){
-                var editor = ace.edit(element);
+                var editor = ace.edit(t.get()[0]);
                 editor.setOptions({
                     maxLines: 20
                 });
